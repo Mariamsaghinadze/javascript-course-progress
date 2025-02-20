639bf83e5119a1f4b211958ceacf69914ecd5ace
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-20 22:58:11 +0400

    Values and variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 8);
+
+let firstName = "Bob"; //camelCase
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415; //constant
+
+let myFirstJob = "Programmer"; //descriptive value
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
