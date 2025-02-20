f09e936cc647284bd96aae20ed8a7c6fc3e96315
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-20 23:18:49 +0400

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,52 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 8);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 8);
 
-let firstName = "Bob"; //camelCase
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Bob"; //camelCase
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let jonas_matilda = "JM";
-let $function = 27;
+// // Variable name conventions
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-let person = "jonas";
-let PI = 3.1415; //constant
+// let person = "jonas";
+// let PI = 3.1415; //constant
 
-let myFirstJob = "Programmer"; //descriptive value
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer"; //descriptive value
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+// //assignment
+
+// let country = "Georgia";
+// let continent = "Europe";
+// let population = 5;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas ");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
