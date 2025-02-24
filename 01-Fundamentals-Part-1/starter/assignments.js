1e0dadf2fe564ae356cb1cb255f81ed49ed1c2db
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-24 23:51:16 +0400

    assignment-strings and template literals

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,39 +1,44 @@
-// // Values and Varibales
+// Values and Varibales
 
-// const country = "Georgia";
-// const continent = "Europe";
-// let population = 5;
+const country = "Georgia";
+const continent = "Europe";
+let population = 5;
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// // Data Types
-// // let, const and var
+// Data Types
+// let, const and var
 
-// const isIsland = false;
-// const language = "Georgian";
+const isIsland = false;
+const language = "Georgian";
 
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
 
-// // Basic Operators
+// Basic Operators
 
-// console.log(population / 2);
-// console.log(population++);
-// const finlandPopulation = 6;
-// console.log(population > finlandPopulation);
-// const averagePopulation = 33;
-// console.log(population < averagePopulation);
-// const description =
-//   country +
-//   " is in " +
-//   continent +
-//   ", and its " +
-//   population +
-//   " million people speak " +
-//   language;
+console.log(population / 2);
+console.log(population++);
+const finlandPopulation = 6;
+console.log(population > finlandPopulation);
+const averagePopulation = 33;
+console.log(population < averagePopulation);
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
 
-// console.log(description);
+console.log(description);
+
+// Strings and template literals
+
+const description1 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description1);
