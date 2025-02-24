f7ea852a73db7d2767f1120df3af4eabfde46a14
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-24 21:55:53 +0400

    section-basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,20 +1,20 @@
-// Values and Varibales
+// // Values and Varibales
 
-const country = "Georgia";
-const continent = "Europe";
-let population = 5;
+// const country = "Georgia";
+// const continent = "Europe";
+// let population = 5;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-// Data Types
-// let, const and var
+// // Data Types
+// // let, const and var
 
-const isIsland = false;
-const language = "Georgian";
+// const isIsland = false;
+// const language = "Georgian";
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
