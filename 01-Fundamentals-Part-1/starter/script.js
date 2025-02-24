8519011a668fdc7ef3ef97fce1076121a78f1ad0
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-24 23:13:39 +0400

    section-Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -63,33 +63,47 @@
 // lastName = "Saghinadze";
 // console.log(lastName);
 
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2**3 means 2 to the pwoer of 3 = 2 * 2 * 2
+
+// const firstName = "Mariam";
+// const lastName = "Saghinadze";
+
+// console.log(firstName + " " + lastName);
+
+// // Assigment operators
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1 = 101
+// x--;
+// x--; // x = x - 1
+// console.log(x);
+
+// // Comparison operators
+
+// console.assert.log(ageJonas > ageSarah); // > , < , >= , <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2019);
+
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2**3 means 2 to the pwoer of 3 = 2 * 2 * 2
-
-const firstName = "Mariam";
-const lastName = "Saghinadze";
-
-console.log(firstName + " " + lastName);
-
-// Assigment operators
-let x = 10 + 5; //15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1 = 101
-x--;
-x--; // x = x - 1
-console.log(x);
-
-// Comparison operators
-
-console.assert.log(ageJonas > ageSarah); // > , < , >= , <=
-console.log(ageSarah >= 18);
-
-const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2019);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+
+console.log(ageJonas, ageSarah, averageAge);
