9b3ffe08782f854eb5658d5bb4c42656d20ca0dc
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-25 00:29:41 +0400

    section-type conversion and coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -132,21 +132,42 @@
 //     multiple
 //     lines`);
 
-const age = 15;
+// const age = 15;
 
-if (age >= 18) {
-  console.log("Sarah can start driving license 🚗");
-} else {
-  const yearsLeft = 18 - age;
-  console.log(`Sarah is too young. Wait another ${yearsLeft} years😊`);
-}
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years😊`);
+// }
 
-const birthYear = 1991;
-let century;
+// const birthYear = 1991;
+// let century;
 
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
-console.log(century);
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+
+console.log(String(23));
+
+// type coercion
+console.log("I am " + 23 + " years old");
+
+console.log("23" - "10" - 3);
+console.log("23" + "10" - 3);
+console.log("23" * "2");
+
+let n = "1" + 1;
+
+n = n - 1;
+console.log(n);
