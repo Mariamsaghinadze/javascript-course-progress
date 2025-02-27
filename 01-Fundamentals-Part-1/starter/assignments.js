128025a33b12e9d36cb1a5c2c8a2a54dd3036c92
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-28 00:44:31 +0400

    assignment-switch statement

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -71,12 +71,33 @@
 //   console.log("No borders!");
 // }
 
-const language = "english";
-const population = 5;
-const isIsland = false;
+// const language = "english";
+// const population = 5;
+// const isIsland = false;
 
-if ((language === "english", population < 50 && !isIsland)) {
-  console.log("You should live in Georgia!");
-} else {
-  console.log("Georgia does not meet your criteria.");
+// if ((language === "english", population < 50 && !isIsland)) {
+//   console.log("You should live in Georgia!");
+// } else {
+//   console.log("Georgia does not meet your criteria.");
+// }
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too...");
 }
