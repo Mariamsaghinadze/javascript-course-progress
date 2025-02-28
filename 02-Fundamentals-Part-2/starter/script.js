b2e6904b344844077927ec9b95630986a63e48db
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-28 14:09:33 +0400

    section-functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,9 +1,30 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriversLicense) console.log("I can drive!");
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log("I can drive!");
 
-const interface = "Audio";
+// const interface = "Audio";
+
+function logger() {
+  console.log("My name is Mariam");
+}
+
+//calling / running function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
