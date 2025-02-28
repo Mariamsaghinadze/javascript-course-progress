6954bdf4c409bd212e3c856fa25989a4a61bae5b
Mariamsaghinadze <mariamsaghinadze15@gmail.com>
2025-02-28 14:49:01 +0400

    assignment-function declaration vs expression

--- 02-Fundamentals-Part-2/starter/assigments.js
+++ 02-Fundamentals-Part-2/starter/assigments.js
@@ -8,3 +8,25 @@ const desPortugal = describeCountry("Portugal", 10, "Lisbon");
 const desGermany = describeCountry("Germany", 83, "Berlin");
 
 console.log(desGeorgia, desGermany, desPortugal);
+
+const populationWorld = 7900;
+
+function percentageOfWorld1(population) {
+  return (population / populationWorld) * 100;
+}
+
+const percantageGeorgia = percentageOfWorld1(5);
+const percantagePortugal = percentageOfWorld1(10);
+const percentageChina = percentageOfWorld1(1441);
+
+console.log(percantageGeorgia, percantagePortugal, percentageChina);
+
+const percentagePopulation = function (population) {
+  return (population / populationWorld) * 100;
+};
+
+console.log(
+  percentagePopulation(5),
+  percentagePopulation(10),
+  percentagePopulation(1441)
+);
